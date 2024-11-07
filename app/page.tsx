b18bad685ba686fed57fa96a6927ea609c133c9a
aisl1n <aislanarchy@gmail.com>
2024-11-06 22:06:44 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <h1 className="tw-flex tw-w-screen tw-justify-center tw-p-5 tw-text-red-500">
+      Hello World
+    </h1>
   );
 }
