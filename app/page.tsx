856226ecdb6c5f288d5a2e18037946d82a63f8a2
aisl1n <aislanarchy@gmail.com>
2024-11-06 22:27:11 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
-  return (
-    <h1 className="tw-flex tw-w-screen tw-justify-center tw-p-5 tw-text-red-500">
-      Hello World
-    </h1>
-  );
+  return <Button> Meu botão la ele </Button>;
 }
