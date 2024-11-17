1163b48ce51f0b30fed515a7dea21cf93ac480fb
aisl1n <aislanarchy@gmail.com>
2024-11-17 16:24:15 -0300

    feat: add stripe webhook that handles subscription

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,15 +1,19 @@
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AquirePlanButton from "./_components/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
+
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
 
   return (
     <>
@@ -18,7 +22,7 @@ const SubscriptionPage = async () => {
         <h1 className="text-2xl font-bold">Assinatura</h1>
         <div className="flex gap-6">
           <Card className="w-[450px]">
-            <CardHeader>
+            <CardHeader className="border-b border-solid py-8">
               <h2 className="text-center text-2xl font-semibold">
                 Plano Básico
               </h2>
@@ -40,7 +44,12 @@ const SubscriptionPage = async () => {
             </CardContent>
           </Card>
           <Card className="w-[450px]">
-            <CardHeader>
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-4 bg-primary/10 text-sm text-primary hover:bg-primary/10">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
@@ -56,7 +65,7 @@ const SubscriptionPage = async () => {
                 <p>Trasações ilimitadas</p>
               </div>
               <div className="flex items-center gap-2">
-                <XIcon className="text-primary" />
+                <CheckIcon className="text-primary" />
                 <p>Relatórios IA</p>
               </div>
               <AquirePlanButton />
